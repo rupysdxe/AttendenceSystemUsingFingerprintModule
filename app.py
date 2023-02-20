76b4280d0d3214f049176ca17197c6ea68349c6a
Rupesh Dangi <83266954+rupysdxe@users.noreply.github.com>
2023-02-20 12:52:46 +0545

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
