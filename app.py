416b682256d14c713031874cca41017617b61527
upretisaurav <upretisaurav7@gmail.com>
2023-02-21 21:28:07 +0545

    Updated the dashboard

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_folder="static")
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template("home.html")
+    return render_template("index.html")
 
 
 
